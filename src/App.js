66834c657b3a13188bc33711b4b615fdd532df15
Dario Santos <daro@Darios-MacBook-Pro.local>
2024-12-07 18:45:00 -0300

    removing google analytic because of chrome policy

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import React from 'react';
 
 import AppProvider from './AppProvider';
 import AppRoutes from './AppRoutes';
-import ReactGA from 'react-ga4';
 import { isExtension } from './utils/platform';
 
 const App = () => {
-  if (!isExtension()) {
-    ReactGA.initialize('G-HE1ZJJBN0V');
-  }
   // Disallow rendering inside an iframe to prevent clickjacking.
   if (window.self !== window.top) {
     return null;
--- src/hooks/useAnalyticsEventTracker.js
+++ src/hooks/useAnalyticsEventTracker.js
@@ -1,15 +1,17 @@
 import { useEffect } from 'react';
-import ReactGA from 'react-ga4';
 import { isExtension } from '../utils/platform';
 
 const useAnalyticsEventTracker = section => {
   useEffect(() => {
+    /*
     if (!isExtension()) {
       ReactGA.send({ hitType: 'pageview', page: section });
     }
+    */
   }, [section]);
 
   const trackEvent = (action, label) => {
+    /*
     if (!isExtension()) {
       ReactGA.event({
         category: section,
@@ -17,6 +19,7 @@ const useAnalyticsEventTracker = section => {
         label: label,
       });
     }
+    */
   };
   return { trackEvent };
 };
